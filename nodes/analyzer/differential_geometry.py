4753d59d8b5ba74b858ec2b0d228c611d75f7ee3
summereasy <51897649+summereasy@users.noreply.github.com>
2021-02-08 04:59:00 +0800

    DDG_geometry Node V0.1 Finished
    Basic Functions
    TODO - Dependencies Trimesh / Wei Utilities
    TODO - MASS / Laplacian Matrix

--- nodes/analyzer/differential_geometry.py
+++ nodes/analyzer/differential_geometry.py
@@ -19,7 +19,8 @@
 import numpy as np
 
 import bpy
-from bpy.props import EnumProperty
+import sys
+from bpy.props import EnumProperty, FloatProperty
 
 import sverchok
 from sverchok.node_tree import SverchCustomTreeNode
@@ -30,7 +31,7 @@ from sverchok.dependencies import scipy
 
 operations = [
     ('GRAD', 'Gradient'),
-    ('DIV/CURL', 'Divergence and Curl')
+    ('DIV_CURL', 'Div and Curl')
     # ('LAPLACE', 'Laplacian Matrix', [('Vertices', 'Polygons')], [('Laplacian Matrix')]),
     # ('MASS', 'Mass Matrix', [('Vertices', 'Polygons')], [('Mass Matrix')])
 ]
@@ -39,6 +40,13 @@ scalar_ops = {'GRAD'}
 
 operation_modes = [(id, name, name, i)
                    for i, (id, name) in enumerate(operations)]
+
+sys.path.append('/opt/anaconda3/envs/trimesh/lib/python3.7/site-packages/')
+import trimesh
+
+sys.path.append('/Users/zhaowei/Documents/Study/DDG/libs')
+from mesh_calculus import gradient, divergence, curl
+
 
 if scipy is None:
     add_dummy('SvDifferentialGeometryNode', "Differential Geometry", 'scipy')
@@ -60,12 +68,13 @@ else:
             self.inputs.new('SvStringsSocket', "Polygons")
             self.inputs.new('SvStringsSocket', "ScalarValue")
             self.inputs.new('SvVerticesSocket', "VectorValue")
-            # for checking purpose
-            self.outputs.new('SvVerticesSocket', "Vertices")
-            self.outputs.new('SvStringsSocket', "Polygons")
-            self.outputs.new('SvVerticesSocket', "Grad")
-            self.outputs.new('SvStringsSocket', "Div")
+            self.inputs.new('SvStringsSocket', "Scale").prop_name = 'scale'
+            self.outputs.new('SvVerticesSocket', "Positions")
+            self.outputs.new('SvVerticesSocket', "Gradient")
+            self.outputs.new('SvStringsSocket', "Divergence")
             self.outputs.new('SvStringsSocket', "Curl")
+            # self.outputs.new('SvVerticesSocket', "Vertices")
+            # self.outputs.new('SvStringsSocket', "Polygons")
             self.update_sockets(context)
 
         def draw_buttons(self, context, layout):
@@ -77,16 +86,22 @@ else:
             if self.operation == 'GRAD':
                 self.inputs['ScalarValue'].hide_safe = False
                 self.inputs['VectorValue'].hide_safe = True
-                self.outputs['Grad'].hide_safe = False
-                self.outputs['Div'].hide_safe = True
+                self.outputs['Gradient'].hide_safe = False
+                self.outputs['Divergence'].hide_safe = True
                 self.outputs['Curl'].hide_safe = True
 
             else:
                 self.inputs['ScalarValue'].hide_safe = True
                 self.inputs['VectorValue'].hide_safe = False
-                self.outputs['Grad'].hide_safe = True
-                self.outputs['Div'].hide_safe = False
+                self.outputs['Gradient'].hide_safe = True
+                self.outputs['Divergence'].hide_safe = False
                 self.outputs['Curl'].hide_safe = False
+
+            updateNode(self, context)
+
+        scale: FloatProperty(
+            name='Size', description='Size of Gradient', default=10.0, update=updateNode)
+        # norm: BoolProperty(name='Center', description='Center the line', default=False, update=updateNode)
 
         operation: EnumProperty(
             name="Operation",
@@ -100,13 +115,45 @@ else:
 
             verts = self.inputs['Vertices'].sv_get(default=[[None]])
             faces = self.inputs['Polygons'].sv_get(default=[[None]])
+            sVals = self.inputs['ScalarValue'].sv_get(default=[[None]])
+            vVals = self.inputs['VectorValue'].sv_get(default=[[None]])
+            scale = self.inputs['Scale'].sv_get(default=[[None]])
+
+            Grads = []
+            Pos = []
+            Div = []
+            Curl = []
+            if self.operation == 'GRAD':
+                for v, p, sVal in zip(verts, faces, sVals):
+                    mesh = trimesh.Trimesh(v, p, process=False)
+                    S = np.array(sVals[0])
+                    G = gradient(S, mesh, rotated=False).T
+                    Gr = gradient(S, mesh, rotated=True).T
+                    tri_centers = mesh.vertices[mesh.faces].mean(axis=1)
+                    Grads.append((tri_centers + G * scale).tolist())
+                    Pos.append(tri_centers.tolist())
+
+            elif self.operation == 'DIV_CURL':
+                for v, p, vVal in zip(verts, faces, vVals):
+                    mesh = trimesh.Trimesh(v, p, process=False)
+                    V = np.array(vVals[0])
+                    D = divergence(V, mesh)
+                    C = curl(V, mesh)
+                    Div.append(D)
+                    Curl.append(C)
 
             # outputs
-            if self.outputs['Vertices'].is_linked:
-                self.outputs['Vertices'].sv_set(verts)
+            if self.outputs['Positions'].is_linked:
+                self.outputs['Positions'].sv_set(Pos)
 
-            if self.outputs['Polygons'].is_linked:
-                self.outputs['Polygons'].sv_set(faces)
+            if self.outputs['Gradient'].is_linked:
+                self.outputs['Gradient'].sv_set(Grads)
+
+            if self.outputs['Divergence'].is_linked:
+                self.outputs['Divergence'].sv_set(Div)
+
+            if self.outputs['Curl'].is_linked:
+                self.outputs['Curl'].sv_set(Curl)
 
 
 def register():
